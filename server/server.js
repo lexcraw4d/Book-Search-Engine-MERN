ca7c922ec45a10d10b51c9e3e36fbbfd3e9e78a9
Lex Crawford <lexcraw4d@gmail.com>
2021-09-13 00:06:13 -0400

    port

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
