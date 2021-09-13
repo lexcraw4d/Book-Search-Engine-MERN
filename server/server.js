b040d5ebe266fc9060eaf8ae59ad1bc419e8a8e3
Lex Crawford <lexcraw4d@gmail.com>
2021-09-13 01:04:31 -0400

    added production env

--- server/server.js
+++ server/server.js
@@ -24,11 +24,18 @@ async function startServer() {
 
 startServer()
 
-// integrate our Apollo server with the Express application as middleware
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+
 
 db.once('open', () => {
   app.listen(process.env.PORT || 3001, () => {
