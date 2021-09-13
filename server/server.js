87cd71b06a428fd3f025616ad934dbdf1c52e2c5
Lex Crawford <lexcraw4d@gmail.com>
2021-09-13 15:39:41 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require('./utils/auth')
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
